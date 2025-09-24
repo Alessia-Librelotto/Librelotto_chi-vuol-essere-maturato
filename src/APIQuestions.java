f0013ed03008ae75b7f1e2b43aa2292db18c6602
Alessia <10934197@itisrossi.vi.it>
2025-09-24 11:30:52 +0200

    Added classes and main

---
+++ src/APIQuestions.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class APIQuestions {
+    public String category;
+    public String type;
+    public String difficulty;
+    public String question;
+    public String correct_answer;
+    public List<String> incorrect_answers;
+}
